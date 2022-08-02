54239284872dba1f8b61e0e23ce7098931e3dd3c
StevenRafft <steven.rafft@gmail.com>
2022-08-02 20:59:05 +0200

    mutator :)

--- levels/teeworlds_dm1_zombies/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Copying
-copy "../../scripts/" "D:\Games\Jazz Jackrabbit 2+\" /y
-copy teeworlds_tsf.j2t "D:\Games\Jazz Jackrabbit 2+\" /y
-copy dm1.j2l "D:\Games\Jazz Jackrabbit 2+\" /y
-copy dm1.j2as "D:\Games\Jazz Jackrabbit 2+\" /y
-echo Starting
-"D:\Games\Jazz Jackrabbit 2+\Jazz2.exe" -server dm1
-@echo on
