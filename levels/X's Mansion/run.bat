9858b00c370c067d45bbd7dac7119877500abb60
StevenRafft <steven.rafft@gmail.com>
2023-01-05 02:19:31 +0200

    aaaaaaaaaaaaaaa

--- levels/X's Mansion/run.bat
+++ levels/X's Mansion/run.bat
@@ -3,6 +3,7 @@ echo Copying
 copy "../../scripts/" "C:\Game\Jazz Jackrabbit 2\createdbyme\" /y
 copy STVmansion.j2l "C:\Game\Jazz Jackrabbit 2\createdbyme\" /y
 copy STVmansion.j2as "C:\Game\Jazz Jackrabbit 2\createdbyme\" /y
+copy test.png "C:\Game\Jazz Jackrabbit 2\createdbyme\" /y
 echo Starting
 "C:\Game\Jazz Jackrabbit 2\Jazz2.exe" -server STVmansion
 @echo on
