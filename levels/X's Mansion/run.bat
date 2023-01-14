1e7a54b048116c443e5f0c5b5925ed104c3543bc
StevenRafft <steven.rafft@gmail.com>
2023-01-15 00:58:02 +0200

    assets

--- levels/X's Mansion/run.bat
+++ levels/X's Mansion/run.bat
@@ -3,8 +3,10 @@ echo Copying
 copy "../../scripts/" "F:\Games\Jazz2\" /y
 copy STVmansion.j2l "F:\Games\Jazz2\" /y
 copy STVmansion.j2as "F:\Games\Jazz2\" /y
-copy test.png "F:\Games\Jazz2\" /y
-copy test2.png "F:\Games\Jazz2\" /y
+cd assets
+copy STVMansion_test.png "F:\Games\Jazz2\" /y
+copy STVMansion_test2.png "F:\Games\Jazz2\" /y
+cd ../
 echo Starting
 "F:\Games\Jazz2\Jazz2+.exe" -server STVmansion
 @echo on
