fa6ba28a930d1fbfaf45660e3cdc62c07ea8044d
StevenRafft <steven.rafft@gmail.com>
2023-01-15 18:01:37 +0200

    weapon list crash fix

--- mutators/weaponlist/run.bat
+++ mutators/weaponlist/run.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Copying
 copy STVweaponlist.mut "F:\Games\Jazz2\" /y
-copy STVweaponlist_constants.asc "F:\Games\Jazz2\" /y
+copy "../../scripts/" "F:\Games\Jazz2\" /y
 echo Starting
 "F:\Games\Jazz2\Jazz2+.exe" -server -mutators=STVweaponlist.mut
 @echo on
