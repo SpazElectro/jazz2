2de3d4a7070c7795b7b3ddca53102ce9a6c3ef68
StevenRafft <steven.rafft@gmail.com>
2023-02-13 01:30:14 +0200

    BETTER

--- generation_scripts/snippets.py
+++ generation_scripts/snippets.py
@@ -1,14 +1,12 @@
 # var abc = document.getElementById("abc")
-
 # var items = []
 # var prevDescription = '';
-
 # function findNextDD(element, startIndex) {
 #   let resultx = "";
 #   let counter = 0;
 #   for (let i = startIndex; i < element.children.length; i++) {
 #     if (element.children[i].tagName === "DD") {
-#       resultx += element.children[i].textContent + " ";
+#       resultx += element.children[i].textContent + "\n";
 #     } else if (element.children[i].tagName === "DT") {
 #       if (prevDescription === resultx) {
 #         continue;
@@ -24,11 +22,11 @@
 #   prevDescription = '';
 #   return resultx;
 # }
-
 # for (let i = 0; i < abc.children.length; i++) {
 #   if (abc.children[i].tagName === "DT") {
 #     const name = abc.children[i].getElementsByClassName("name")[0].id;
 #     const description = findNextDD(abc, i + 1);
+#     if(name == "") continue;
 #     items.push({
 #       name: name,
 #       description: description
@@ -41,6 +39,13 @@ import json
 result = {}
 
 for item in json.load(open("api.json")):
+    result[item["name"]] = {
+        "prefix": item["name"],
+        "body": item["name"],
+        "description": item["description"]
+    }
+
+for item in json.load(open("apifunctions.json")):
     result[item["name"]] = {
         "prefix": item["name"],
         "body": item["name"],
