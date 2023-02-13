0d08f64fa33b6cae7b6d4c6d4889f5ec8191f3ac
StevenRafft <steven.rafft@gmail.com>
2023-02-13 02:27:56 +0200

    gg

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 this is where i work on mutators, levels, scripts, etc
 
 you can check my jazz2online profile [here](https://www.jazz2online.com/users/22073/spaz-electro)
+
+## setup
+most important thing: Visual Studio ****Code****
+
+### building the snippets/autocomplete
+go to generation_scripts, open snippets.py (python 3)
+copy the outputted file `snippets_build.json` and go to the .vscode folder
+paste it and name it "plus.code-snippets" (yes, remove the json and make it code-snippets)
+and you're done!
+
+### good syntax highlight
+download this extension for vsc: C/C++ Extension Pack
--- generation_scripts/snippets.py
+++ generation_scripts/snippets.py
@@ -1,9 +1,11 @@
-# var abc = document.getElementById("abc")
+# abc = document.getElementById("abc")
 # var items = []
 # var prevDescription = '';
+
 # function findNextDD(element, startIndex) {
 #   let resultx = "";
 #   let counter = 0;
+
 #   for (let i = startIndex; i < element.children.length; i++) {
 #     if (element.children[i].tagName === "DD") {
 #       resultx += element.children[i].textContent + "\n";
@@ -11,28 +13,50 @@
 #       if (prevDescription === resultx) {
 #         continue;
 #       }
+
 #       prevDescription = resultx;
 #       break;
 #     }
+
 #     counter++;
+
 #     if (counter === 10) {
 #       break;
 #     }
 #   }
+
 #   prevDescription = '';
+
 #   return resultx;
 # }
+
 # for (let i = 0; i < abc.children.length; i++) {
 #   if (abc.children[i].tagName === "DT") {
 #     const name = abc.children[i].getElementsByClassName("name")[0].id;
 #     const description = findNextDD(abc, i + 1);
+#     var full = "";
+
+#     for (let it = 0; it < abc.children[i].children.length; it++) {
+#         if(abc.children[i].nodeType == 3) {
+#             full += abc.children[i].nodeValue
+#         } else {
+#             full += abc.children[i].innerText
+#         }
+
+#         if(full.includes(")")) break;
+#     }
+    
 #     if(name == "") continue;
+
 #     items.push({
 #       name: name,
-#       description: description
+#       description: description,
+#       full: full
 #     });
 #   }
 # }
+
+# console.log(items)
 
 import json
 
@@ -48,9 +72,9 @@ for item in json.load(open("api.json")):
 for item in json.load(open("apifunctions.json")):
     result[item["name"]] = {
         "prefix": item["name"],
-        "body": item["name"],
+        "body": item["full"],
         "description": item["description"]
     }
 
-with open("snippets.json", "w") as f:
+with open("snippets_build.json", "w") as f:
     json.dump(result, f)
