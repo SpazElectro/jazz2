121c8501ee0c3247f9b776740b15fa26045977cd
StevenRafft <steven.rafft@gmail.com>
2023-02-14 14:42:56 +0200

    abc

--- generation_scripts/iconbuilder/build.py
+++ generation_scripts/iconbuilder/build.py
@@ -11,24 +11,24 @@ animationSettings = """default:
 
 frameId = 0
 
-try: shutil.rmtree("./j2a/icons-j2a")
+try: shutil.rmtree("../j2a/icons-j2a")
 except Exception: pass
 
-os.mkdir("./j2a/icons-j2a")
-os.mkdir("./j2a/icons-j2a/0")
-os.mkdir("./j2a/icons-j2a/0/0")
+os.mkdir("../j2a/icons-j2a")
+os.mkdir("../j2a/icons-j2a/0")
+os.mkdir("../j2a/icons-j2a/0/0")
 
 xid = 0
 
 mainIndex = 0
 subIndex = 0
 
-with open(f"./j2a/icons-j2a/{mainIndex}/{subIndex}/animation.settings", "w") as animsettings:
+with open(f"../j2a/icons-j2a/{mainIndex}/{subIndex}/animation.settings", "w") as animsettings:
     animsettings.write(animationSettings)
     animsettings.close()
 
 for x in os.listdir("../../icons/"):
-    shutil.copyfile(f"../../icons/{x}", f"./j2a/icons-j2a/{mainIndex}/{subIndex}/frame-{xid:03}.png")
+    shutil.copyfile(f"../../icons/{x}", f"../j2a/icons-j2a/{mainIndex}/{subIndex}/frame-{xid:03}.png")
     
     print(f"Built {x} in icons-j2a/{mainIndex}/{subIndex}/frame-{xid:03}")
 
@@ -37,18 +37,18 @@ for x in os.listdir("../../icons/"):
     if xid >= 1000:
         subIndex += 1
         xid = 0
-        os.mkdir(f"./j2a/icons-j2a/{mainIndex}/{subIndex}")
+        os.mkdir(f"../j2a/icons-j2a/{mainIndex}/{subIndex}")
         
-        with open(f"./j2a/icons-j2a/{mainIndex}/{subIndex}/animation.settings", "w") as animsettings:
+        with open(f"../j2a/icons-j2a/{mainIndex}/{subIndex}/animation.settings", "w") as animsettings:
             animsettings.write(animationSettings)
             animsettings.close()
     if subIndex >= 100:
         mainIndex += 1
         subIndex = 0
         xid = 0
-        os.mkdir(f"./j2a/icons-j2a/{mainIndex}/{subIndex}")
+        os.mkdir(f"../j2a/icons-j2a/{mainIndex}/{subIndex}")
 
-os.system("python ./j2a/j2a-import.py \"./j2a/icons-j2a\"")
+os.system("python ../j2a/j2a-import.py \"../j2a/icons-j2a\"")
 
 prepareInSD = input("Do you want to open JazzSD with this animation file? (Y/n)")
 
@@ -56,7 +56,7 @@ if prepareInSD == "" or prepareInSD.lower() == "y":
     if os.path.exists("../../sprites/Anims.j2a"):
         os.rename("../../sprites/Anims.j2a", "../../sprites/cache/Anims.j2a.old-" + str(randint(1, 10000)))
     
-    shutil.copyfile("./j2a/icons.j2a", "../../sprites/Anims.j2a")
+    shutil.copyfile("../j2a/icons.j2a", "../../sprites/Anims.j2a")
 
     os.chdir("../../sprites/")
     os.startfile("JazzSD.exe")
--- generation_scripts/j2anamer/name.py
+++ generation_scripts/j2anamer/name.py
@@ -3,8 +3,8 @@ import os
 namesInOrder = open("list").read().split("\n")
 nameId = 0
 
-for x in os.listdir("../iconbuilder/j2a/Anims-j2a"):
-    os.rename(f"../iconbuilder/j2a/Anims-j2a/{x}/", f"../iconbuilder/j2a/Anims-j2a/{namesInOrder[nameId]}/")
+for x in os.listdir("../j2a/Anims-j2a"):
+    os.rename(f"../j2a/Anims-j2a/{x}/", f"../j2a/Anims-j2a/{namesInOrder[nameId]}/")
     
     print(f"Renamed {x} to {namesInOrder[nameId]}")
     
