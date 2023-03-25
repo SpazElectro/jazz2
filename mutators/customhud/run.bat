892bbfe31d625f4e858fab269122e7219972e2b2
StevenRafft <steven.rafft@gmail.com>
2023-03-25 16:32:38 +0200

    fix util

---
+++ mutators/customhud/run.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Copying
+copy "../../scripts/" "F:\Games\Jazz2\" /y
+copy STVcustomhud.mut "F:\Games\Jazz2\" /y
+echo Starting
+"F:\Games\Jazz2\Jazz2+.exe" -server -mutators=STVcustomhud.mut battle1
+@echo on
