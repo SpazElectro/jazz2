4c8d6567ac98a590911e50330e9e8917e30a62b4
StevenRafft <steven.rafft@gmail.com>
2023-04-17 20:42:54 +0200

    improvements

--- mutators/customhud/run.bat
+++ mutators/customhud/run.bat
@@ -3,5 +3,5 @@ echo Copying
 copy "../../scripts/" "F:\Games\Jazz2\" /y
 copy STVcustomhud.mut "F:\Games\Jazz2\" /y
 echo Starting
-"F:\Games\Jazz2\Jazz2+.exe" -server -mutators=STVcustomhud.mut battle1
+"F:\Games\Jazz2\Jazz2+.exe" -server -mutators=STVcustomhud.mut castle1
 @echo on
