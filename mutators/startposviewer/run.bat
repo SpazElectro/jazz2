56159e4837624be789f6b6ba7290ae5e25af7fa4
StevenRafft <steven.rafft@gmail.com>
2023-04-19 13:03:59 +0200

    bacds

--- mutators/startposviewer/run.bat
+++ mutators/startposviewer/run.bat
@@ -1,6 +1,33 @@
 @echo off
-echo Copying
-copy STVstartposviewer.mut "F:\Games\Jazz2\" /y
+
+REM Read variables from run.ini
+for /f "tokens=1* delims==" %%a in ('type "..\..\run.ini" ^| find "="') do (
+  if /i "%%a"=="GAME_DIRECTORY" set "GAME_DIRECTORY=%%b"
+  if /i "%%a"=="GAME_NAME" set "GAME_NAME=%%b"
+)
+
+echo Copying files...
+copy "../../scripts/" "%GAME_DIRECTORY%" /y
+for %%i in (*.j2l *.j2as *.mut) do (
+  copy "%%i" "%GAME_DIRECTORY%" /y
+)
+
+set "J2L_LEVEL="
+set "MUTATOR="
+
 echo Starting
-"F:\Games\Jazz2\Jazz2+.exe" -server -mutators=STVstartposviewer.mut face
+for %%i in (*.j2l) do (
+  set "J2L_LEVEL=%%i"
+  goto :check_mutator
+)
+
+:check_mutator
+for %%i in (*.mut) do (
+  set "MUTATOR=-mutators=%%i"
+  goto :start_game
+)
+
+:start_game
+if not defined J2L_LEVEL set "J2L_LEVEL=battle1"
+"%GAME_DIRECTORY%%GAME_NAME%" -server %MUTATOR% %J2L_LEVEL% -battle
 @echo on
