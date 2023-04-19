31e2d018e24323be9cfc9ecbc67b50ffeedfaec3
StevenRafft <steven.rafft@gmail.com>
2023-04-19 13:06:53 +0200

    more

--- levels/SpazLife/run_cli.bat
+++ levels/SpazLife/run_cli.bat
@@ -1,4 +1,11 @@
 @echo off
 echo Starting
-"F:\Games\Jazz2\Jazz2+.exe" -connect 127.0.0.1
+
+REM Read variables from run.ini
+for /f "tokens=1* delims==" %%a in ('type "..\..\run.ini" ^| find "="') do (
+  if /i "%%a"=="GAME_DIRECTORY" set "GAME_DIRECTORY=%%b"
+  if /i "%%a"=="GAME_NAME" set "GAME_NAME=%%b"
+)
+
+"%GAME_DIRECTORY%%GAME_NAME%" -connect 127.0.0.1
 @echo on
