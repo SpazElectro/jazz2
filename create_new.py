5aeb3a7011bdee6c935e0469ecfc51837a4698b8
StevenRafft <steven.rafft@gmail.com>
2023-04-21 17:47:24 +0200

    tests with rabbit npc

--- create_new.py
+++ create_new.py
@@ -7,7 +7,7 @@ name = input("Name? ")
 run = """@echo off
 
 REM Read variables from run.ini
-for /f "tokens=1* delims==" %%a in ('type "..\..\run.ini" ^| find "="') do (
+for /f "tokens=1* delims==" %%a in ('type "..\..\\run.ini" ^| find "="') do (
   if /i "%%a"=="GAME_DIRECTORY" set "GAME_DIRECTORY=%%b"
   if /i "%%a"=="GAME_NAME" set "GAME_NAME=%%b"
 )
