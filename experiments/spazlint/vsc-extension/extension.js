76a38900d8947117bfa34646139b294d33f9ba6f
StevenRafft <steven.rafft@gmail.com>
2023-11-26 00:10:27 +0200

    working

--- experiments/spazlint/vsc-extension/extension.js
+++ experiments/spazlint/vsc-extension/extension.js
@@ -7,13 +7,13 @@ let writeFileAsync = promisify(writeFile);
 
 const serverAddress = "localhost";
 const serverPort = 17338;
-const client = new net.Socket();
+var client = new net.Socket();
 
 let connectedToServer = false;
 let dontReconnect = false;
 let pythonChildProcess = null;
 
-function reconnectToServer(attempt=0) {
+function reconnectToServer(attempt = 0) {
     client.connect(serverPort, serverAddress, () => {
         connectedToServer = true;
         vscodeButton.text = "$(megaphone) Stop";
@@ -21,85 +21,110 @@ function reconnectToServer(attempt=0) {
 
         vscode.window.showInformationMessage("Connected to spazlint!");
     });
-    
+
     client.on("close", () => {
-        if(attempt == 3) {
-            return vscode.window.showInformationMessage("Attempt #3 to connect to spazlint has failed!");
+        if (attempt == 3) {
+            return vscode.window.showInformationMessage(
+                "Attempt #3 to connect to spazlint has failed!"
+            );
         }
 
-        if(dontReconnect) {
+        if (dontReconnect) {
             dontReconnect = false;
             return;
         }
 
         console.log("Connection was closed!");
 
-        // if(!connectedToServer) {
-        //     vscode.window.showInformationMessage("Attempting to reconnect to spazlint...");
-            
-        //     pythonChildProcess = execFile("python", [
-        //         process.env["SPAZLINT_DIR"] + "\\main.py"
-        //     ], (error, stdout, stderr) => {
-        //         if (error) {
-        //             // this is when the process gets killed
-        //             if(error.message.startsWith("Command failed: python ")) {
-        //                 vscode.window.showInformationMessage("Spazlint server has successfully stopped!");
-        //                 return;
-        //             } else {
-        //                 vscode.window.showErrorMessage(error.message);
-        //             }
-                    
-        //             reject(error.message);
-        //         } else if (stderr) {
-        //             vscode.window.showErrorMessage(stderr);
-        //             reject(stderr);
-        //         } else {
-        //             resolve(stdout);
-        //         }
-        //     });
-        
-        //     setTimeout(() => {
-    //         reconnectToServer(attempt + 1);
-    //     }, 3_000);
-    // } else {
-        //     vscode.window.showInformationMessage("Disconnected from spazlint!");
-        // }
-        
+        if (!connectedToServer) {
+            vscode.window.showInformationMessage(
+                "Attempting to reconnect to spazlint..."
+            );
+
+            pythonChildProcess = execFile(
+                "python",
+                [process.env["SPAZLINT_DIR"] + "\\main.py"],
+                (error, stdout, stderr) => {
+                    if (error) {
+                        // this is when the process gets killed
+                        if (
+                            error.message.startsWith("Command failed: python ")
+                        ) {
+                            vscode.window.showInformationMessage(
+                                "Spazlint server has successfully stopped!"
+                            );
+                            return;
+                        } else {
+                            vscode.window.showErrorMessage(error.message);
+                        }
+
+                        reject(error.message);
+                    } else if (stderr) {
+                        vscode.window.showErrorMessage(stderr);
+                        reject(stderr);
+                    } else {
+                        resolve(stdout);
+                    }
+                }
+            );
+
+            setTimeout(() => {
+                reconnectToServer(attempt + 1);
+            }, 3_000);
+        } else {
+            vscode.window.showInformationMessage("Disconnected from spazlint!");
+        }
+
         connectedToServer = false;
     });
 }
 
 function stopServer() {
-    if(pythonChildProcess != null) {
+    client.destroy();
+
+    if (pythonChildProcess != null) {
         dontReconnect = true;
         pythonChildProcess.kill();
         vscodeButton.text = "$(megaphone) Connect";
         vscodeButton.tooltip = "Connect to spazlint!";
-    } else vscode.window.showErrorMessage("Attempted to stop server when python process has already been killed!")
+    } else
+        vscode.window.showErrorMessage(
+            "Attempted to stop server when python process has already been killed!"
+        );
 }
 
 var extensionDiagnostics;
 
 function getDefaultOf(type) {
-    if (["int", "uint", "int8", "uint8", "int16", "uint16", "int64", "uint64", "float", "double"].includes(type))
+    if (
+        [
+            "int",
+            "uint",
+            "int8",
+            "uint8",
+            "int16",
+            "uint16",
+            "int64",
+            "uint64",
+            "float",
+            "double",
+        ].includes(type)
+    )
         return "0";
-    if (type == "bool")
-        return "false";
+    if (type == "bool") return "false";
     if (type.startsWith("array<"))
         return "array<" + type.split("array<")[1].split(">")[0] + ">() = {}";
-    if (type == "string")
-        return "\"\"";
+    if (type == "string") return '""';
 
     return "undefined";
 }
 
 function getFileLocation() {
-    var location = vscode.window.activeTextEditor.document.uri.fsPath
+    var location = vscode.window.activeTextEditor.document.uri.fsPath;
 
-    while(location.includes("\\"))
-        location = location.replace("\\", "_")
-    location = location.replace(":", "_")
-    
+    while (location.includes("\\")) location = location.replace("\\", "_");
+    location = location.replace(":", "_");
+
     return process.env["SPAZLINT_DIR"] + "\\temp\\" + location + ".tmp";
 }
 
@@ -109,26 +134,23 @@ async function runPythonScript(adv = false) {
             type: adv === true ? "ADVANCED_AUTOCOMPLETE" : "AUTOCOMPLETE",
             line: vscode.window.activeTextEditor.selection.active.line.toString(),
             char: vscode.window.activeTextEditor.selection.active.character.toString(),
-            file: getFileLocation()
+            file: getFileLocation(),
         };
-
-        console.log("Running script!");
 
         let accumulatedData = "";
 
         const onDataReceived = (data) => {
-            console.log(`chunkRecv, data: ${typeof(data)}; dataStringLength: ${data.toString().length}, accum: ${accumulatedData.length}`);
             accumulatedData += data.toString();
 
             try {
                 JSON.parse(accumulatedData.split("\n")[0]);
                 JSON.parse(accumulatedData.split("\n")[1]);
-                console.log("Full recv!");
                 resolve(accumulatedData);
                 accumulatedData = "";
                 client.off("data", onDataReceived);
             } catch (error) {
-                console.log(`ERR: ${error}`);
+                if (!(error instanceof SyntaxError))
+                    console.log(`ERR: ${error}`);
             }
         };
 
@@ -144,47 +166,80 @@ let completion = {
             const suggestions = JSON.parse(output.split("\n")[0]);
 
             var completionItems = suggestions.map((suggestion) => {
+                let isEvent = suggestion["type"] == "event";
                 let x = new vscode.CompletionItem(
                     suggestion["name"],
-                    suggestion["type"] == "function" ?
-                        vscode.CompletionItemKind.Function : (suggestion["type"] == "property" ? vscode.CompletionItemKind.Property : vscode.CompletionItemKind.Variable)
+                    suggestion["type"] == "function" || isEvent
+                        ? vscode.CompletionItemKind.Function
+                        : suggestion["type"] == "property"
+                            ? vscode.CompletionItemKind.Property
+                            : vscode.CompletionItemKind.Variable
                 );
 
-                x.documentation = suggestion["description"] + (suggestion["type"] == "function" ? ("\n\n" + suggestion["full"]) : "");
+                x.documentation =
+                    suggestion["description"] +
+                    (suggestion["type"] == "function" || isEvent
+                        ? "\n\n" + suggestion["full"]
+                        : "");
 
-                if (suggestion["type"] == "function") {
+                if (suggestion["type"] == "function" || isEvent) {
                     let str = suggestion["name"] + "(";
                     let ss = new vscode.SnippetString(str);
 
                     if (suggestion["items"] != undefined) {
                         var i = 0;
 
-                        suggestion["items"].map(x => {
+                        suggestion["items"].map((x) => {
                             if (x.items != undefined && x.items.length != 0) {
-                                let newItems = [];
+                                if (isEvent) {
+                                    ss.appendText(x.type + " " + x.name);
+                                } else {
+                                    let newItems = [];
 
-                                x.items.forEach(itm => {
-                                    newItems.push((x.type.split("::")[0] == "jjBEHAVIOR" ? "BEHAVIOR" : x.type.split("::")[0]) + "::" + itm);
-                                });
+                                    x.items.forEach((itm) => {
+                                        newItems.push(
+                                            (x.type.split("::")[0] ==
+                                                "jjBEHAVIOR"
+                                                ? "BEHAVIOR"
+                                                : x.type.split("::")[0]) +
+                                            "::" +
+                                            itm
+                                        );
+                                    });
 
-                                ss.appendText(x.name + ": ");
-                                ss.appendChoice(newItems);
+                                    ss.appendText(x.name + ": ");
+                                    ss.appendChoice(newItems);
+                                }
                             } else {
-                                ss.appendText(x.name + ": ");
-                                if (!x.defaultValue && getDefaultOf(x.type) == "\"\"") {
-                                    ss.appendText("\"");
-                                    ss.appendPlaceholder("");
-                                    ss.appendText("\"");
-                                } else
-                                    ss.appendPlaceholder(x.defaultValue || getDefaultOf(x.type));
+                                if (isEvent) {
+                                    ss.appendText(x.type + " " + x.name);
+                                } else {
+                                    ss.appendText(x.name + ": ");
+
+                                    if (
+                                        !x.defaultValue &&
+                                        getDefaultOf(x.type) == '""'
+                                    ) {
+                                        ss.appendText('"');
+                                        ss.appendPlaceholder("");
+                                        ss.appendText('"');
+                                    } else {
+                                        ss.appendPlaceholder(
+                                            x.defaultValue ||
+                                            getDefaultOf(x.type)
+                                        );
+                                    }
+                                }
                             }
 
-                            ss.appendText(i == suggestion["items"].length - 1 ? "" : ", ");
+                            ss.appendText(
+                                i == suggestion["items"].length - 1 ? "" : ", "
+                            );
                             i += 1;
                         });
                     }
 
-                    ss.appendText(");");
+                    ss.appendText(isEvent ? ") {\n\n}" : ");");
 
                     x.insertText = ss;
                 }
@@ -198,21 +253,53 @@ let completion = {
             return [];
         }
     },
-}
+};
 
-async function refreshDiagnostics(advanced=false) {
-    if((vscode.window.activeTextEditor == null || vscode.window.activeTextEditor.document.isUntitled || !(vscode.window.activeTextEditor.document.fileName.endsWith(".j2as") || vscode.window.activeTextEditor.document.fileName.endsWith(".mut") || vscode.window.activeTextEditor.document.fileName.endsWith(".asc"))))
+async function refreshDiagnostics(advanced = false) {
+    if (
+        vscode.window.activeTextEditor == null ||
+        vscode.window.activeTextEditor.document.isUntitled ||
+        !(
+            vscode.window.activeTextEditor.document.fileName.endsWith(
+                ".j2as"
+            ) ||
+            vscode.window.activeTextEditor.document.fileName.endsWith(".mut") ||
+            vscode.window.activeTextEditor.document.fileName.endsWith(".asc")
+        )
+    )
         return;
-        
-    await writeFileAsync(getFileLocation(), vscode.window.activeTextEditor.document.getText());
+
+    await writeFileAsync(
+        getFileLocation(),
+        vscode.window.activeTextEditor.document.getText()
+    );
 
     try {
         const output = await runPythonScript(advanced);
         const errors = JSON.parse(output.split("\n")[1]);
 
-        var diagnostics = errors.map((diagnostic) => new vscode.Diagnostic(new vscode.Range(diagnostic["line"], diagnostic["char"], diagnostic["line"] + 1, 0), diagnostic["text"], diagnostic["type"] == "ERR" ? vscode.DiagnosticSeverity.Error : (diagnostic["type"] == "INFO" ? vscode.DiagnosticSeverity.Information : vscode.DiagnosticSeverity.Warning)));
+        var diagnostics = errors.map(
+            (diagnostic) =>
+                new vscode.Diagnostic(
+                    new vscode.Range(
+                        diagnostic["line"],
+                        diagnostic["char"],
+                        diagnostic["line"] + 1,
+                        0
+                    ),
+                    diagnostic["text"],
+                    diagnostic["type"] == "ERR"
+                        ? vscode.DiagnosticSeverity.Error
+                        : diagnostic["type"] == "INFO"
+                            ? vscode.DiagnosticSeverity.Information
+                            : vscode.DiagnosticSeverity.Warning
+                )
+        );
 
-        extensionDiagnostics.set(vscode.window.activeTextEditor.document.uri, diagnostics)
+        extensionDiagnostics.set(
+            vscode.window.activeTextEditor.document.uri,
+            diagnostics
+        );
     } catch (error) {
         console.error("Error running error linter:", error);
         return [];
@@ -222,13 +309,21 @@ async function refreshDiagnostics(advanced=false) {
 var vscodeButton;
 
 function activate(context) {
-    extensionDiagnostics = vscode.languages.createDiagnosticCollection("jj2plus");
+    extensionDiagnostics =
+        vscode.languages.createDiagnosticCollection("jj2plus");
     context.subscriptions.push(extensionDiagnostics);
 
-    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics()));
-    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(e => refreshDiagnostics(true)));
-    
-    vscodeButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 101);
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeTextDocument((e) => refreshDiagnostics())
+    );
+    context.subscriptions.push(
+        vscode.workspace.onDidSaveTextDocument((e) => refreshDiagnostics(true))
+    );
+
+    vscodeButton = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Right,
+        101
+    );
     vscodeButton.text = "$(megaphone) Connect";
     vscodeButton.command = "spazlint.connect";
     vscodeButton.tooltip = "Connect to spazlint!";
@@ -236,7 +331,7 @@ function activate(context) {
 
     context.subscriptions.push(
         vscode.commands.registerCommand("spazlint.connect", () => {
-            if(!connectedToServer) {
+            if (!connectedToServer) {
                 reconnectToServer();
             } else {
                 stopServer();
@@ -246,9 +341,9 @@ function activate(context) {
 
     vscode.languages.registerCompletionItemProvider("cpp", completion);
 
-    console.log("[SpazLint] Registered completion items!")
+    console.log("[SpazLint] Registered completion items!");
 }
 
 function deactivate() { }
 
-module.exports = { activate, deactivate }
+module.exports = { activate, deactivate };
